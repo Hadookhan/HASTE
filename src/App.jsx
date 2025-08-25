436b5e69df5c441e42b9e0ee06f4b4d9da2df6c7
hadxr <hadikhan05@hotmail.co.uk>
2025-08-25 16:27:05 +0100

    v1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import hasteLogo from './assets/haste.png'
+import './App.css'
+
+function App() {
+//  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='mainHeader'>HASTE</h1>
+      </div>
+      <div className="card">
+        <p>Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
